474556b44054588904203edb46e6e43fb645414b
Kmilo Denis Glez <kmilo.denis.glez@gmail.com>
2022-08-24 11:28:58 -0400

    added setenv.sh
    Signed-off-by: Kmilo Denis Glez <kmilo.denis.glez@gmail.com>

---
+++ setenv.sh
@@ -0,0 +1,8 @@
+# only dev mode
+# to set execute in the linux bash the command:
+# source setenv.sh
+
+export HLF_DAPP_CONFIG="/mnt/d/Source/Go/src/drones/conf.linux.yaml"
+
+# You can leave these variables like this, remember only in dev mode
+export HLF_DAPP_JWT_SIGN_KEY="secret__sample__with__32__chars_"
