bc89488101726c8aa955ae35ebf4d90d10db35ff
Kmilo Denis Glez <kmilo.denis.glez@gmail.com>
2022-08-24 20:51:01 -0400

    created validation.go
    Signed-off-by: Kmilo Denis Glez <kmilo.denis.glez@gmail.com>

---
+++ lib/validation.go
@@ -0,0 +1,39 @@
+package lib
+
+import (
+	"fmt"
+	"github.com/kmilodenisglez/drones.restapi/schema/dto"
+
+	"github.com/asaskevich/govalidator"
+	reg "regexp"
+)
+
+// ValidateMedicationCodeArray validate a string collection given a regular expression
+func ValidateMedicationCodeArray(data []interface{}, regexp string) bool {
+	var fn govalidator.ConditionIterator = func(value interface{}, index int) bool {
+		fmt.Println(value.(string))
+		return reg.MustCompile(regexp).MatchString(value.(string))
+	}
+	return govalidator.ValidateArray(data, fn)
+}
+
+// InitValidator Activate behavior to require all fields and adding new validators
+func InitValidator() {
+	govalidator.SetFieldsRequiredByDefault(false)
+
+	// Add your own struct validation tags
+	// validates medication name
+	govalidator.TagMap["medication_name_validation"] = func(str string) bool {
+		return reg.MustCompile(dto.RegexpMedicationName).MatchString(str)
+	}
+
+	// validates medication code
+	govalidator.TagMap["medication_code_validation"] = func(str string) bool {
+		return reg.MustCompile(dto.RegexpMedicationCode).MatchString(str)
+	}
+
+	// validates that an enum is within the interval
+	govalidator.TagMap["drone_enum_validation"] = func(str string) bool {
+		return str != "unknown"
+	}
+}
